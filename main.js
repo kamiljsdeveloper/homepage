4653148d5c6c8846a8c340eb82ef1305585a2ff2
kamiljsdeveloper <kamiljsdeveloper@gmail.com>
2023-09-01 14:33:50 +0200

    Learning CSS i JS

---
+++ main.js
@@ -0,0 +1,9 @@
+const names = "Kamil";
+const nazwisko = "Jacewicz";
+const wiek = 34;
+
+alert("Joł Joł");
+
+console.log(names);
+console.log(nazwisko);
+console.log(`Nazywam się ${names} ${nazwisko} i mam ${wiek} lat`)
